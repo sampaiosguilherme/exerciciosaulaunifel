d6676295cb70c71992aa18cd139a2fccc2ac65b3
Guilherme Sampaio <sampaio.programacao@gmail.com>
2023-10-15 16:38:58 -0300

    testando isso aqui

---
+++ exercicios27092023/batman/script.js
@@ -0,0 +1,6 @@
+let valor1 = 5;  //  criação
+const valor2 = 15
+
+valor1 =  18   // reatribuição
+
+console.log(valor1 + valor2);
---
+++ javascript11/10/2023/script.js
@@ -0,0 +1,7 @@
+let conta = 10  
+
+const conta2 = 15  
+
+conta2 = 18 
+
+console.log(conta2);

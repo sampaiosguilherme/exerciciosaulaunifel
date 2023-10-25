a987f3807e7bf62a5d4d7d34d770b6833bc6408a
Guilherme Sampaio <sampaio.programacao@gmail.com>
2023-10-25 10:53:28 -0300

    atualizado25/10/2023-faltasolucao

--- javascript11/10/2023/script.js
+++ javascript11/10/2023/script.js
@@ -1,14 +1,31 @@
-const valor = [10, 8, 50, 63, 41, 71, 2]
-
-const soma = (array) => {
-    let total = 0
-
-    for (let index = 0; index < array.length; index += 1) {
-        total += array[index]
+const order = {
+    name: 'Rafael Andrade',
+    phoneNumber: '11-98763-1416',
+    address: {
+      street: 'Rua das Flores',
+      number: '389',
+      apartment: '701',
+    },
+    order: {
+      pizza: {
+        marguerita: {
+          amount: 1,
+          price: 25,
+        },
+        pepperoni: {
+          amount: 1,
+price: 20,
+        },
+      },
+      drinks: {
+        coke: {
+          type: 'Coca-Cola Zero',
+          price: 10,
+          amount: 1,
+        },
+      },
+      delivery: {
+        deliveryPerson: 'Ana Silveira',
+      }
     }
-
-    return total
 }
-
-console.log(soma(valor))
-    
